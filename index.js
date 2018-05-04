cdd61fc9ab428f94f50352816078dac14d74a793
Justin - PC <justinjoyce88@gmail.com>
2018-05-04 11:27:55 -0400

    Added animation. Some syntax fixes

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-$(document).ready(function () {
-  $('#generate-random-quote').on('click', function (e) {
+$(() => {
+  $("#generate-random-quote").on("click", function(e) {
     $.ajax({
       url: "https://talaikis.com/api/quotes/random/",
-      success: function (data) {
+      success: data => {
         tweet = data;
-        $('.quote').html('<em>"' + data.quote + '"</em>');
-        $('.quote-author').html("- " + data.author);
+        $(".quote").html('<em>"' + data.quote + '"</em>');
+        $(".quote-author").html("- " + data.author);
       },
       cache: false
     });
   });
 
-  $('.tweet').on('click', function () {
-    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + tweet.quote);
+  $(".tweet").on("click", function() {
+    $(this).attr(
+      "href",
+      "https://twitter.com/intent/tweet?text=" + tweet.quote
+    );
+  });
+
+  $("#generate-random-quote").click(function() {
+    // restart animation
+    var me = this;
+    this.style.webkitAnimation = "none";
+    setTimeout(() => (me.style.webkitAnimation = ""), 10);
+    setTimeout(() => me.blur(), 1000);
   });
 });
